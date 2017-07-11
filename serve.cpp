2b127c27bca64846e3a820c16012657808e42c43
deanturpin <git@lvbtn.uk>
2017-07-11 13:45:47 +0100

    report content length in http header

--- serve.cpp
+++ serve.cpp
@@ -4,17 +4,22 @@ int main() {
 
 	using namespace std;
 
-	// HTTP header
-	cout << "Content-Type: text/text\n" << endl;
-
-	// The good stuff
-	cout <<
+	const string message =
 		R"(
 			{
-				"firstName": "Dean",
+				"firstName": "Mr.",
+				"middleName": "Dean",
 				"lastName": "Turpin"
 			}
 		)";
+
+	// HTTP header
+	cout << "Content-Type: text/text" << endl;
+	cout << "Content-Length: " << message.size() + 1 << endl;
+	cout << "" << endl;
+
+	// The good stuff
+	cout << message << endl;
 
 	return 0;
 }

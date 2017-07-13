abe0056bf117d4e10d07877434483f35b3da0f0e
deanturpin <git@lvbtn.uk>
2017-07-13 18:39:33 +0100

    generate lots more plots

--- serve.cpp
+++ serve.cpp
@@ -1,52 +1,39 @@
+#include <sstream>
 #include <iostream>
 
 int main() {
 
 	using namespace std;
 
-	const string message =
-		R"(
-{
-	"type": "alpha",
-	"packets": [
-		{
-			"one": "0.1",
-			"two": "0.2",
-			"ten": "0.1"
-		},
-		{
-			"one": "0.1",
-			"two": "0.3",
-			"ten": "0.1"
-		},
-		{
-			"one": "0.3",
-			"two": "0.2",
-			"ten": "0.4"
-		},
-		{
-			"one": "0.3",
-			"two": "0.6",
-			"ten": "0.8"
-		},
-		{
-			"one": "0.5",
-			"two": "0.8",
-			"ten": "0.11"
-		}
-	]
-}
-		)";
-
 	// HTTP header
 	cout
 		<< "Content-Type: text/plain\n"
-		<< "Content-Length: " << to_string(message.size() + 1) + "\n"
+		// << "Content-Length: " << to_string(message.size() + 1) + "\n"
 		<< "Connection: Closed\n"
 		<< "\n";
 
+	stringstream message;
+
+	message << R"({ "type": "alpha", "packets": [)";
+
+	const int totalPackets = 1000;
+
+	for (int i = 0; i < totalPackets; ++i) {
+
+		message << "{";
+		message << "\"one\": \"" << 1 << "\",";
+		message << "\"two\": \"" << 1 << "\",";
+		message << "\"ten\": \"" << 1 << "\"";
+		message << "}";
+
+		if (i < totalPackets - 1)
+			message << ",";
+	}
+
+	message << R"(]})";
+
 	// The good stuff
-	cout << message << endl;
+	cout << message.str() << endl;
 
 	return 0;
 }
